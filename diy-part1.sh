8dba7158e9f434f4b3060984c665f5518a4a711c
676077263 <676077263@qq.com>
2023-09-10 13:22:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,9 @@
 # Add a feed source
 # echo 'src-git small https://github.com/shiyu1314/onecloud-package' >>feeds.conf.default
 
+# Add a feed source
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf ../lean/luci-theme-argon
